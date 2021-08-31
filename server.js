1e643a396de83797a828f1a175e525e3332f7c3d
unknown <wojtekgg93@gmail.com>
2021-08-31 18:41:48 +0200

    sufficient layout

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const app = express();
 
-
 app.set('view engine', 'ejs');
+
+app.use(express.static('views'));
 
 app.get('/', (req, res)=>{
     res.render('index');
---
+++ views/script.js
@@ -0,0 +1,8 @@
+const sidebar = document.getElementById('sidebar');
+const toggleButton = document.getElementById('toggleButton');
+
+// console.log(document.body.offsetWidth)
+// sidebar.addEventListener('hidden.bs.collapse', ()=>{
+//     console.log('hidden');
+       
+// });
