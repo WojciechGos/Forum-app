0f074a2a0a79ef36e2c6cadee11465e37d97e039
unknown <wojtekgg93@gmail.com>
2021-10-19 14:35:00 +0200

    sending post req from fetch working

--- controllers/EntryController.js
+++ controllers/EntryController.js
@@ -1,6 +1,12 @@
 
 function entryPost(req, res){
-
+    console.log(req.body)
+    res.redirect()
+}
+function entryGet(req, res){
+    
 }
 
+
 module.exports.entryPost = entryPost
+module.exports.entryGet = entryGet
--- routes/EntryRouter.js
+++ routes/EntryRouter.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const router = express.Router()
 const entryPost = require('../controllers/EntryController').entryPost
+const entryGet = require('../controllers/EntryController').entryGet
+const upload = require('../config/multer').upload
 
-router.post('/post', entryPost)
+router.post('/entry', upload.none(), entryPost)
+router.get('/entry', entryGet)
 
 
 module.exports = router
