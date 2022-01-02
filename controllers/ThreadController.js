13c99e8b15cda05f50936924c1472f3c19f2c94f
unknown <wojtekgg93@gmail.com>
2022-01-02 17:02:14 +0100

    fixed sending data through form

--- controllers/ThreadController.js
+++ controllers/ThreadController.js
@@ -1,17 +1,17 @@
 const Thread = require('../model/Thread')
 
 async function threadPost(req, res){
-    console.log(req.body)
     try{
         const thread = new Thread({
             title: req.body.title,
             description: req.body.description
         })
-        console.log(thread)
+        console.log(`thread obj ${thread} `)
         await thread.save()
     }
     catch(e){
         console.error(e)
+        res.status(500).send({error:"boo"})
     }
 }
 
--- model/Thread.js
+++ model/Thread.js
@@ -1,10 +1,7 @@
 const mongoose = require('mongoose')
 
 const threadSchema = new mongoose.Schema({
-    title:{
-        type:String,
-        required: true  
-    },
+    title:String,
     description: String,
 })
 
