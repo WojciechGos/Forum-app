1e1662884ae756bd962a0a941cc97b1947a8009e
unknown <wojtekgg93@gmail.com>
2022-03-09 07:07:14 +0100

    login: error messeges are accurate

--- routes/LoginRouter.js
+++ routes/LoginRouter.js
@@ -6,15 +6,32 @@ const upload = require('../config/multer').upload
 
 router.get('/login', loginGet);
 
-// router.post('/login', passport.authenticate('local', {
-//     successRedirect: '/',
-//     failureRedirect: '/',
-//     failureFlash: true
-// }))
+router.post('/login', upload.none(), (req, res, next) => {
 
-router.post('/login', upload.none(), (req, res)=>{
-    console.log(req.body)
-    res.json({succes: false})
+    // console.log()
+    if(req.body.username === '' && req.body.password ===''){
+        res.json({ succes: false, error: 'Brak danych uwierzytelniających' })
+        return
+    }
+    passport.authenticate('local', (err, user, info)=>{
+        if(err){
+            console.log(err)
+            return next(err)
+        }
+        if(!user){
+
+            res.json({succes: false, error: info.message})
+        }else{
+            console.log('succes login')
+
+            res.render('index')
+        }
+    })(req, res, next)
 })
+
+// router.post('/login', upload.none(), (req, res)=>{
+//     console.log(req.body)
+//     res.json({succes: false})
+// })
     
 module.exports = router;
