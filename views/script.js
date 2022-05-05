21b2fc8d6ba1f83214dfc52a08dabc6b3ffc61ec
unknown <wojtekgg93@gmail.com>
2022-05-05 12:41:08 +0200

    problem with eventListener: it doesnt working with AddEntry function

--- views/script.js
+++ views/script.js
@@ -1,11 +1,4 @@
-const sidebar = document.getElementById('sidebar');
-const toggleButton = document.getElementById('toggleButton');
-const loginTab = document.getElementById('loginTab')
-const registerTab = document.getElementById('registerTab')
-const formModal = document.getElementById('formModal')
-const createPostButton = document.getElementById('create_post')
-const createThreadButton = document.getElementById('create_thread')
-const createPostInput = document.getElementById('createPostInput')
+
 
 // window.addEventListener("paste", function (e) {
 //     var item = Array.from(e.clipboardData.items).find(x => /^image\//.test(x.type));
